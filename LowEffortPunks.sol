621d3513756d97ebe501e85e889103308ae6d028
Ken Preiser <kenpreiser@gmail.com>
2022-08-22 15:47:31 -0700

    Update LowEffortPunks.sol

--- LowEffortPunks.sol
+++ LowEffortPunks.sol
@@ -1,24 +1,64 @@
-// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity ^0.8.7;
+pragma solidity 0.8.7;
 
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "./ERC721A.sol";
-import "./IRenderer.sol";
-import "./IOpenseaStorefront.sol";
 
-contract LowEffortPunks is Ownable, ERC721A, Pausable {
-    IOpenseaStorefront os;
-    IRenderer renderer;
+interface Metadata {
+    function tokenURI(uint256 tokenId) external returns (string memory);
+}
 
-    uint256 migrateTilToken = 0xC0C8D886B92A811E8E41CB6AB5144E44DBBFBFA3000000000015C00000000001;
+contract LEP is ERC721('Low Effort Punks', 'LEP'), Ownable, ERC1155Holder, ReentrancyGuard, Pausable {
 
-    constructor(address _renderer, address _os, uint256 _migrateTilToken) ERC721A("Low Effort Punks", "LEP") {
-        os = IOpenseaStorefront(_os);
-        renderer = IRenderer(_renderer);
-        migrateTilToken = _migrateTilToken;
+    address OS = 0x495f947276749Ce646f68AC8c248420045cb7b5e;
+    address MD;
+    bool gasRefundEnabled;
+    uint256 refundInWei;
+
+    modifier onlyFren {
+        //frens can map.
+        require(frens[msg.sender] || msg.sender == owner(), "not a fren");
+        _;
+    }
+
+    mapping(uint256 => bool) isIdMapped;
+    mapping(uint256 => uint256) map;
+    mapping(address => bool) frens;
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC1155Receiver) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function onERC1155Received(
+            address operator, address from, uint256 id, uint256 value, bytes calldata data
+    ) public virtual override returns (bytes4) {
+        require(operator == OS, "not an os token");
+        require(isIdMapped[id], "token not mapped");
+        require(!paused(), "paused");
+        require(!gasRefundEnabled || tx.gasprice < 11 gwei, "gas refund is enabled but gas is too high");
+        _safeMint(from, map[id]);
+        return this.onERC1155Received.selector;
+    }
+
+    function isLepReadyToMigrate(uint256 osTokenId) external view returns (bool) {
+        return isIdMapped[osTokenId];
+    }
+    function isFren(address _user) external view returns (bool) {
+        return frens[_user];
+    }
+    function isGasRefundEnabled() external view returns (bool) {
+        return gasRefundEnabled;
+    }
+
+    function mapLep(uint256 osTokenId, uint256 newTokenId) external onlyFren {
+        require(!gasRefundEnabled || tx.gasprice < 11 gwei, "gas refund is enabled but gas is too high");
+        isIdMapped[osTokenId] = true;
+        map[osTokenId] = newTokenId;
     }
 
     function pause() external onlyOwner {
@@ -29,26 +69,26 @@ contract LowEffortPunks is Ownable, ERC721A, Pausable {
         _unpause();
     }
 
-    function migrateFromOpensea(uint256 osTokenId) external whenNotPaused {
-        require(osTokenId >= 0xC0C8D886B92A811E8E41CB6AB5144E44DBBFBFA30000000000018B0000000001, "not a LEP");
-        require(osTokenId <= 0xC0C8D886B92A811E8E41CB6AB5144E44DBBFBFA3000000000015C00000000001, "not a LEP");
+    function retrieveOldLep(uint256 tokenId) external onlyOwner {
+        IERC1155(OS).safeTransferFrom(address(this), msg.sender, tokenId, 1, '');
+    }
+    function retrieveNewLep(uint256 tokenId) external onlyOwner {
+        safeTransferFrom(address(this), msg.sender, tokenId);
     }
 
-    function convertOSTokenIdToNewTokenId(uint256 osTokenId) public view returns (uint256) {
-		uint256 _id = (osTokenId & 0x0000000000000000000000000000000000000000ffffffffffffff0000000000) >> 40;
-        //offset magic needed... doesnt line up 1:1
-        return _id;
+    function setOpenSeaContract(address _os) external onlyOwner {
+        OS = _os;
     }
-
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return renderer.tokenURI(tokenId);
+    function setMetadataContract(address _md) external onlyOwner {
+        MD = _md;
     }
-
-    function setOS(address _newOS) external onlyOwner {
-        os = IOpenseaStorefront(_newOS);
+    function toggleGasRefundEnabled() external onlyOwner {
+        gasRefundEnabled = !gasRefundEnabled;
     }
-
-    function setRenderer(address _newRenderer) external onlyOwner {
-        renderer = IRenderer(_newRenderer);
+    function setRefundInWei(uint256 _amount) external onlyOwner {
+        refundInWei = _amount;
+    }
+    function addFren(address fren) external onlyOwner {
+        frens[fren] = true;
     }
 }
