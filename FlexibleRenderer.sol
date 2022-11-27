d377e160fa9d8aa419534cd4da8cc2cac1a48652
Ken Preiser <kenpreiser@gmail.com>
2022-11-27 15:08:48 -0800

    Update FlexibleRenderer.sol

--- FlexibleRenderer.sol
+++ FlexibleRenderer.sol
@@ -1,49 +1,32 @@
 pragma solidity 0.8.7;
 
-import "./Base64.sol";
-import "./SSTORE2.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "solady/src/utils/Base64.sol";
+import "solady/src/utils/SSTORE2.sol";
+import "solady/src/auth/OwnableRoles.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract Metadata is Ownable {
+contract Metadata is OwnableRoles {
     using Strings for uint256;
 
-    mapping(uint256 => bool) public isPunkOnChain;
     mapping(uint256 => address) onChainPunk;
-    bool public gasRefundEnabled = true;
 
-    modifier onlyFren {
-        //frens can map.
-        require(frens[msg.sender] || msg.sender == owner(), "not a fren");
-        _;
-    }
-
-    mapping(address => bool) frens;
+    uint256 LEP_CONTRACT = _ROLE_1;
 
     function tokenURI(uint256 tokenId) public view returns (string memory) {
         return buildMetadata(tokenId);
     }
 
     function buildMetadata(uint256 tokenId) public view returns(string memory) {
-        if (onChainPunk[tokenId] == address(0)) {
-            return string(abi.encodePacked(
+        return string(abi.encodePacked(
                 'data:application/json;base64,', Base64.encode(bytes(abi.encodePacked(
                             '{"name": "low effort punk ', tokenId.toString(), 
-                            '", "image": "', 
-                            'https://storage.googleapis.com/lep123/', tokenId.toString(), '.png',
-                            '"}')))));
-        } else {
-            return string(abi.encodePacked(
-                'data:application/json;base64,', Base64.encode(bytes(abi.encodePacked(
-                            '{"name": "low effort punk ', tokenId.toString(), 
-                            '", "image": "', 
+                            '", "image": "data:application/json;base64,', 
                             SSTORE2.read(onChainPunk[tokenId]),
                             '"}')))));
-        }
     }
 
     function onChainLep(uint256 tokenId) external view returns (string memory) {
-        return string(SSTORE2.read(onChainPunk[tokenId]));
+        return string(abi.encodePacked('data:application/json;base64,',SSTORE2.read(onChainPunk[tokenId])));
     }
 
     function onChainLepAddress(uint256 tokenId) external view returns (address) {
@@ -52,26 +35,7 @@ contract Metadata is Ownable {
 
     receive() external payable { }
 
-    function putPunkOnChain(uint256 tokenId, string calldata svg) external onlyFren {
-        uint gasAtStart = gasleft();
+    function putPunkOnChain(uint256 tokenId, string calldata svg) external onlyOwnerOrRoles(LEP_CONTRACT) {
         onChainPunk[tokenId] = SSTORE2.write(bytes(svg));
-        if (gasRefundEnabled) {
-            require(tx.gasprice < 11 gwei, "too high");
-            uint256 refund = tx.gasprice * (gasAtStart - gasleft());
-            payable(msg.sender).call{value: refund}('');
-        }
     }
-
-    function isFren(address _user) external view returns (bool) {
-        return frens[_user];
-    }
-
-    function toggleFren(address _user) external onlyOwner {
-        frens[_user] = !frens[_user];
-    }
-
-    function toggleGasRefund() external onlyOwner {
-        gasRefundEnabled = !gasRefundEnabled;
-    }
-
 }
