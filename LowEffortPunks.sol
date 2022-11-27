b727090e5ba93945aa3547326704ee1b05abe338
Ken Preiser <kenpreiser@gmail.com>
2022-11-27 15:09:01 -0800

    Update LowEffortPunks.sol

--- LowEffortPunks.sol
+++ LowEffortPunks.sol
@@ -8,30 +8,30 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "solady/src/auth/OwnableRoles.sol";
 
 interface Metadata {
     function tokenURI(uint256 tokenId) external view returns (string memory);
+    function putPunkOnChain(uint256 tokenId, string calldata svg) external;
 }
 
-contract LEP is ERC721('Low Effort Punks', 'LEP'), Ownable, ERC1155Receiver, ReentrancyGuard, Pausable {
+contract LEP is ERC721('Low Effort Punks', 'LEP'), OwnableRoles, ERC1155Receiver, ReentrancyGuard, Pausable {
 
     //address OS = 0x495f947276749Ce646f68AC8c248420045cb7b5e;
     address OS = 0x88B48F654c30e99bc2e4A1559b4Dcf1aD93FA656;
     address MD = 0x005A20Ba09425A3F9A0dDAf1B0764e9CAF0E8dfc;
     bool gasRefundEnabled;
-    uint256 refundInWei = 150752;
-    uint256 refundForMappingInWei = 82781;
-    uint256 maxGasForRefund = 11 gwei;
-
-    modifier onlyFren {
-        //frens can map.
-        require(frens[msg.sender] || msg.sender == owner(), "not a fren");
-        _;
-    }
+    uint256 maxGweiForRefund = 11 gwei;
+    
+    uint256 FREN_ROLE = _ROLE_1;
 
     mapping(uint256 => bool) isIdMapped;
-    mapping(uint256 => uint256) map;
-    mapping(address => bool) frens;
+    mapping(uint256 => uint256) map;    
+
+
+    constructor() {
+        _initializeOwner(msg.sender);
+    }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         return Metadata(MD).tokenURI(tokenId);
@@ -43,16 +43,10 @@ contract LEP is ERC721('Low Effort Punks', 'LEP'), Ownable, ERC1155Receiver, Ree
 
     function onERC1155Received(
             address operator, address from, uint256 id, uint256 value, bytes calldata data
-    ) public virtual override nonReentrant returns (bytes4) {
+    ) public virtual override nonReentrant whenNotPaused returns (bytes4) {
         require(msg.sender == OS, "not an os token");
         require(isIdMapped[id], "token not mapped");
-        require(!paused(), "paused");
         _safeMint(from, map[id]);
-
-        if (gasRefundEnabled) {
-            require(tx.gasprice < maxGasForRefund, "gas refund enabled but gas too high"); 
-            payable(from).call{value: tx.gasprice * refundInWei}('');
-        }
 
         return this.onERC1155Received.selector;
     }
@@ -65,23 +59,28 @@ contract LEP is ERC721('Low Effort Punks', 'LEP'), Ownable, ERC1155Receiver, Ree
 
     receive() external payable {}
 
+    function getRenderer() external view returns (address) {
+        return MD;
+    }
+
     function isLepReadyToMigrate(uint256 osTokenId) external view returns (bool) {
         return isIdMapped[osTokenId];
     }
-    function isFren(address _user) external view returns (bool) {
-        return frens[_user];
-    }
+
     function isGasRefundEnabled() external view returns (bool) {
         return gasRefundEnabled;
     }
 
-    function mapLep(uint256 osTokenId, uint256 newTokenId) external onlyFren {
+    function mapLep(uint256 osTokenId, uint256 newTokenId, string memory svg) external onlyOwnerOrRoles(FREN_ROLE) {
+        uint gasAtStart = gasleft();        
         isIdMapped[osTokenId] = true;
         map[osTokenId] = newTokenId;
         if (gasRefundEnabled) {
-            require(tx.gasprice < maxGasForRefund, "gas refund enabled but gas too high"); 
-            payable(msg.sender).call{value: tx.gasprice * refundForMappingInWei}('');
-        }   
+            require(tx.gasprice < maxGweiForRefund, "too high");
+            uint256 refund = tx.gasprice * (gasAtStart - gasleft());
+            Metadata(MD).putPunkOnChain(newTokenId, svg);
+            payable(msg.sender).call{value: refund}('');
+        }
     }
 
     function pause() external onlyOwner whenNotPaused {
@@ -95,11 +94,8 @@ contract LEP is ERC721('Low Effort Punks', 'LEP'), Ownable, ERC1155Receiver, Ree
     function retrieveOldLep(uint256 tokenId) external onlyOwner {
         IERC1155(OS).safeTransferFrom(address(this), msg.sender, tokenId, 1, '');
     }
-    function forceMint(uint256 newTokenId, address _user) external onlyOwner {
-        _safeMint(_user, newTokenId);
-    }
-    function retrieveNewLep(uint256 tokenId) external onlyOwner {
-        safeTransferFrom(address(this), msg.sender, tokenId);
+    function forceMint(address _user, uint256 newTokenId) external onlyOwner {
+        _safeMint(_user, newTokenId, '');
     }
 
     function setOpenSeaContract(address _os) external onlyOwner {
@@ -111,19 +107,11 @@ contract LEP is ERC721('Low Effort Punks', 'LEP'), Ownable, ERC1155Receiver, Ree
     function toggleGasRefundEnabled() external onlyOwner {
         gasRefundEnabled = !gasRefundEnabled;
     }
-    function setRefundInWei(uint256 _amount) external onlyOwner {
-        refundInWei = _amount;
-    }
     function setMaxPriceForRefundInWei(uint256 _amount) external onlyOwner {
-        maxGasForRefund = _amount;
+        maxGweiForRefund = _amount;
     }
-    function setRefundForMappingInWei(uint256 _amount) external onlyOwner {
-        refundForMappingInWei = _amount;
-    }
-    function addFren(address fren) external onlyOwner {
-        frens[fren] = true;
-    }
-    function withdraw() external onlyOwner {
+
+    function withdrawETH() external onlyOwner {
         payable(owner()).call{value: address(this).balance}('');
     }
 }
